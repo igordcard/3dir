ce1284c2f07997332b4349abbe2d3b4a1b98f0f8
Igor D.C <igordcard@gmail.com>
2019-01-13 14:09:48 -0800

    Initial batch of files
    Just beginning of development of this Google Chrome extension.

---
+++ 3dir/background.js
@@ -0,0 +1,5 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([]);
+    })
+});
