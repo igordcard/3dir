59f1af430d8ec3b712a3f325169b1ebcf38b6cd3
Igor D.C <igordcard@gmail.com>
2019-01-21 16:54:11 -0800

    Move downloaded file to domain directory
    A first glimpse of what this is supposed to do.
    
    Will create a directory with the same name as the FQDN
    and move the download file inside.
    
    Later this will be configurable and allow for other conditions.

--- 3dir/background.js
+++ 3dir/background.js
@@ -1,6 +1,20 @@
+function getDomainName(url) {
+    // TODO rough version for now
+    return url.split('//')[1].split('/')[0].split(':')[0].split('?')[0];
+}
+
+function getFileName(url) {
+    // TODO rough version for now
+    return url.split('/').pop();
+}
+
 chrome.downloads.onDeterminingFilename.addListener(
     function(downloadItem, suggest) {
         console.log("A new download has started: " + downloadItem.url);
-        suggest({filename: "renamed_download"});
+
+        domainName = getDomainName(downloadItem.url);
+        fileName = getFileName(downloadItem.url);
+
+        suggest({filename: domainName + "/" + fileName});
     }
 )
