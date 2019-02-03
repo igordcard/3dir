c900a19770c1152685f3dd27cddd2651544786c3
Igor D.C <igordcard@gmail.com>
2019-02-02 23:49:15 -0800

    Add option to choose whether to use a subdir
    This only adds the option to the option page as well as
    actually storing the chosen option in chrome sync storage.
    
    Later I will make the extension read this option to use the
    "3dir" subdirectory or not.

--- 3dir/options.js
+++ 3dir/options.js
@@ -1,1 +1,11 @@
-/* :) */
+document.getElementById("subdir").addEventListener('change', (event) => {
+    if (event.target.checked) {
+        chrome.storage.sync.set({subdir: true}, function() {
+            console.log("Using 3dir subdirectory.");
+        });
+    } else {
+        chrome.storage.sync.set({subdir: false}, function() {
+            console.log("Not using 3dir subdirectory.");
+        });
+    }
+  })
